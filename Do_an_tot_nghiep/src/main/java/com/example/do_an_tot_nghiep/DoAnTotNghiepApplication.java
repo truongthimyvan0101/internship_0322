89b13ccce1aea0c59426fbf79c43aade3945f742
Vân Trương <truongthimyvan9@gmail.com>
2022-04-22 13:24:45 +0700

    up

---
+++ Do_an_tot_nghiep/src/main/java/com/example/do_an_tot_nghiep/DoAnTotNghiepApplication.java
@@ -0,0 +1,13 @@
+package com.example.do_an_tot_nghiep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoAnTotNghiepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoAnTotNghiepApplication.class, args);
+    }
+
+}
---
+++ Do_an_tot_nghiep/src/test/java/com/example/do_an_tot_nghiep/DoAnTotNghiepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.do_an_tot_nghiep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoAnTotNghiepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
